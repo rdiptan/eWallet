408d8d79686865c2436249e090bb0fb7d87e4f23
rdiptan <drt347826@gmail.com>
2022-01-03 22:42:41 +0545

    404 error handled

--- app.js
+++ app.js
@@ -12,4 +12,19 @@ app.use(userRoute);
 const adminRoute = require("./routes/adminRoute");
 app.use(adminRoute);
 
+const transactionRoute = require("./routes/transactionRoute");
+app.use(transactionRoute);
+
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
+
+app.all("/*", (req, res) => {
+  res.status(404).end("Page not Found");
+//   res.status(404).json({
+//     success: false,
+//     data: "404",
+//   });
+});
+
 app.listen(90);
---
+++ routes/transactionRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = new express.Router();
+const jwt = require("jsonwebtoken");
+const auth = require("../auth/auth");
+
+
+
+module.exports = router;
