f13d344967aa315a3268034e897d9d9c7dea8a46
rdiptan <drt347826@gmail.com>
2022-01-10 20:14:54 +0545

    view transaction

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -21,24 +21,68 @@ const newTransactionController = (req, res) => {
       category: category,
       reason: reason,
     });
-    transaction_data.save().then(function () {
-      userdetail.findOne({ user: from_id }).then(function (from_data) {
-        if (from_data.balance < data.balance) {
-          res.json({ msg: "Insufficient Balance", success: false });
-          return;
-        }
-        from_data.balance = data.balance - transaction_amount;
-        from_data.save().then(function () {
-          userdetail.findOne({ user: to_id }).then(function (to_data) {
-            to_data.balance = data.balance + transaction_amount;
-            to_data.save().then(function () {
-              res.send({ msg: "Transaction Successful", success: true });
-            });
+    transaction_data
+      .save()
+      .then(function () {
+        userdetail
+          .findOne({ user: from_id })
+          .then(function (from_data) {
+            if (from_data.balance < data.balance) {
+              res.json({ msg: "Insufficient Balance", success: false });
+              return;
+            }
+            from_data.balance = data.balance - transaction_amount;
+            from_data
+              .save()
+              .then(function () {
+                userdetail
+                  .findOne({ user: to_id })
+                  .then(function (to_data) {
+                    to_data.balance = data.balance + transaction_amount;
+                    to_data
+                      .save()
+                      .then(function () {
+                        res.send({
+                          msg: "Transaction Successful",
+                          success: true,
+                        });
+                      })
+                      .catch(function (err) {
+                        res.json({ msg: err, success: false });
+                      });
+                  })
+                  .catch(function (err) {
+                    res.json({ msg: err, success: false });
+                  });
+              })
+              .catch(function (err) {
+                res.json({ msg: err, success: false });
+              });
+          })
+          .catch(function (err) {
+            res.json({ msg: err, success: false });
           });
-        });
+      })
+      .catch(function (err) {
+        res.json({ msg: err, success: false });
       });
-    });
   });
 };
 
-module.exports = { newTransactionController };
+const viewTransactionController = (req, res) => {
+  const user_id = req.userInfo._id;
+  transaction
+    .find({ $or: [{ from: user_id }, { to: user_id }] })
+    .then(function (data) {
+      if (data === null) {
+        res.send({ msg: "No Transaction Found", success: false });
+        return;
+      }
+      res.send({ data, success: true });
+    })
+    .catch(function (err) {
+      res.send({ msg: err, success: false });
+    });
+};
+
+module.exports = { newTransactionController, viewTransactionController };
--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = new express.Router();
 const auth = require("../middleware/auth");
 
-const {newTransactionController} = require("../controllers/transactionController");
+const {newTransactionController, viewTransactionController} = require("../controllers/transactionController");
 
 router.post("/user/newtransaction", auth.verifyUser, newTransactionController);
+router.get("/user/transactions", auth.verifyUser, viewTransactionController);
 
 module.exports = router;
