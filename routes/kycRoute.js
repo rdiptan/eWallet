ca773e625fc12d8927ce22cc5daa9c9bcb0ac9c7
rdiptan <drt347826@gmail.com>
2022-01-21 23:37:21 +0545

    transactions

--- routes/kycRoute.js
+++ routes/kycRoute.js
@@ -5,9 +5,13 @@ const auth = require("../middleware/auth");
 const {
   kycViewController,
   kycVerifyController,
+  unsuccessfulTransactionController,
+  addBalanceController,
 } = require("../controllers/kycController");
 
 router.get("/view", auth.verifyAdmin, kycViewController);
 router.put("/verify:id", auth.verifyAdmin, kycVerifyController);
+router.get("/unsuccessful", auth.verifyAdmin, unsuccessfulTransactionController);
+router.post("/addBalance", auth.verifyAdmin, addBalanceController);
 
 module.exports = router;
